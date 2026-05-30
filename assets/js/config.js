6ad37c17bbf5e6225b743706a176925417c71886
huntersbrhts-beep <huntersbrhts@gmail.com>
2026-05-30 09:39:18 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://yawousokoaqeyvktjosj.supabase.co";
-const SUPABASE_KEY = "sb_publishable__TXuxjZ8PObBZxsM-PjF1Q_fAvPsDa3";
-const WHATSAPP_NUMERO = "5531984656166";
-const BUCKET_IMAGENS = "imagens-produtos";
-
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
